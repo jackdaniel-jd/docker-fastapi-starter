59ac01cf97ffc5312ff549a34fc24a88840fe33b
vigo <vigopattaraphong@gmail.com>
2025-09-13 14:23:09 +0700

    assigment1

--- app/main.py
+++ app/main.py
@@ -20,9 +20,7 @@ async def health_check():
 @app.get("/", tags=["Pages"])
 async def read_root(request: Request):
     sample_cards = [
-        {"name": "Wichy", "description": "Software Engineer"},
-        {"name": "Ai", "description": "Software Engineer"},
-        {"name": "Bank", "description": "Software Engineer"},
+        {"name": "Vigo", "description": "Hello"},
     ]
     return templates.TemplateResponse(
         "index.html",
